6434810c3a98e901ca0013219d2693f6bd2021f0
Shenna Risqianto Wilfred Piri <shennawew@outlook.com>
2020-12-14 23:00:00 +0100

    now not using Lumo.class Theme
    Signed-off-by: Shenna Risqianto Wilfred Piri <shennawew@outlook.com>

--- src/main/java/com/shennarwp/m900/Application.java
+++ src/main/java/com/shennarwp/m900/Application.java
@@ -2,8 +2,6 @@ package com.shennarwp.m900;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -16,7 +14,7 @@ import org.vaadin.artur.helpers.LaunchUtil;
  *
  */
 @SpringBootApplication
-@Theme(value = Lumo.class, variant = Lumo.DARK)
+//@Theme(value = Lumo.class, variant = Lumo.DARK)
 @PWA(name = "dashboard", shortName = "dashboard")
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
