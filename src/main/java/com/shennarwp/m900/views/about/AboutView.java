f7b01f95b54104d97da33ee6caccbfc18331e988
Shenna Risqianto Wilfred Piri <shennawew@outlook.com>
2020-12-23 00:17:46 +0100

    revert some commit containing API
    Signed-off-by: Shenna Risqianto Wilfred Piri <shennawew@outlook.com>

--- src/main/java/com/shennarwp/m900/views/about/AboutView.java
+++ src/main/java/com/shennarwp/m900/views/about/AboutView.java
@@ -1,10 +1,10 @@
 package com.shennarwp.m900.views.about;
 
+import com.shennarwp.m900.views.main.MainView;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.shennarwp.m900.views.main.MainView;
 
 @Route(value = "about", layout = MainView.class)
 @PageTitle("About")
@@ -12,6 +12,7 @@ public class AboutView extends Div {
 
     public AboutView() {
         setId("about-view");
+        setWidth("80%");
         add(new Text("Content placeholder"));
     }
 
