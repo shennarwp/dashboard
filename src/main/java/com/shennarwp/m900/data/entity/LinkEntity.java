1d438db5094fda32b700836abcd01171ac036024
Shenna Risqianto Wilfred Piri <shennawew@outlook.com>
2020-12-28 03:57:34 +0100

    POJO for Link-Object
    Signed-off-by: Shenna Risqianto Wilfred Piri <shennawew@outlook.com>

---
+++ src/main/java/com/shennarwp/m900/data/entity/LinkEntity.java
@@ -0,0 +1,42 @@
+package com.shennarwp.m900.data.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "LINKENTITY")
+@Getter @Setter
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class LinkEntity
+{
+	@Id @NonNull @GeneratedValue (strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Integer id;
+
+	@NonNull
+	@Column(name = "TITLE")
+	private String title;
+
+	@NonNull
+	@Column(name = "URL")
+	private String url;
+
+	@Column(name = "CATEGORY")
+	private String category;
+
+	@Column(name = "IMAGENAME")
+	private String imageName;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof LinkEntity))
+			return false;
+		LinkEntity other = (LinkEntity) obj;
+
+		return getTitle().equals(other.getTitle()) &&
+				getUrl().equals(other.getUrl()) &&
+				getImageName().equals(other.getImageName());
+	}
+}
